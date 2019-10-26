e5f3c32598783bd373c203531880fa407ff56a4d
Andrew Weiss <wandrew8@gmail.com>
2019-10-25 17:51:01 -0700

    Updates

---
+++ precache-manifest.42b7324f99d487e280aae0e963e08b88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccaf167da306f9e21e052e299fe332b",
+    "url": "/memory-click-game/index.html"
+  },
+  {
+    "revision": "44a894fb604768b952d6",
+    "url": "/memory-click-game/static/css/main.f35b9287.chunk.css"
+  },
+  {
+    "revision": "2cc1274490ae2fd2e3d8",
+    "url": "/memory-click-game/static/js/2.86b6421a.chunk.js"
+  },
+  {
+    "revision": "44a894fb604768b952d6",
+    "url": "/memory-click-game/static/js/main.6d0a19b0.chunk.js"
+  },
+  {
+    "revision": "0822df9be56784ea4494",
+    "url": "/memory-click-game/static/js/runtime-main.6e5a36ce.js"
+  }
+]);
